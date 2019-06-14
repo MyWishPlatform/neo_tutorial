62ce4b8a4b93aa28280c0cc6b17dfbcbc7d245c4
Dmitriy Kovalyov <dmitriy.kovalyov@ddgcorp.ru>
2019-06-14 15:31:01 +0300

    Administration pages

--- neo_tutorial-frontend/js/admin/login.js
+++ neo_tutorial-frontend/js/admin/login.js
@@ -1,3 +1,23 @@
-angular.module('adminApp').controller('AdminAuthController', ['$scope', function($scope) {
-    $scope.adminStartText = 'Start Auth Text';
+angular.module('authApp', []).run(['$rootScope', function($rootScope) {
+
+}]).config(['$httpProvider', '$qProvider', '$compileProvider', function($httpProvider, $qProvider, $compileProvider) {
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $qProvider.errorOnUnhandledRejections(false);
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(mailto|otpauth|https?):/);
 }]);
+
+
+angular.module('authApp').controller('AdminAuthController', ['$scope', '$http', function($scope, $http) {
+    $scope.request = {};
+    $scope.sendAuthForm = function () {
+        $http.post('/administration/login/', $scope.request).then(function(response) {
+            console.log(response);
+        });
+        console.log($scope.request);
+    };
+    return false;
+
+}]);
+
+
--- neo_tutorial/administration/urls.py
+++ neo_tutorial/administration/urls.py
@@ -6,6 +6,8 @@ from neo_tutorial.administration.views import AdministrationView, Administration
 urlpatterns = [
     path('', AdministrationView.as_view()),
     path('login/', AdministrationLoginView.as_view()),
-    path('users/', get_all_users),
-    path('users/add/', UserAddView.as_view())
+    path('users/', get_all_users, name='admin-users'),
+    path('users/add/', UserAddView.as_view(), name='admin-users-add'),
+    path('courses/', AdministrationView.as_view(), name='admin-courses'),
+    path('glossary/', AdministrationView.as_view(), name='admin-glossary'),
 ]
