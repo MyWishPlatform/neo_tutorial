cdf9a287129e9a2d80f756250c2334381e1e6be2
Maksim Strenk <ephdtrg@gmail.com>
2019-06-17 16:29:00 +0300

    fix course models, add 400 if user already exists

---
+++ neo_tutorial/courses/api.py
@@ -0,0 +1,4 @@
+from .models import BasicCourse, LessonContent, CourseMaterial, Lesson, Test, Speciality
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
--- neo_tutorial/courses/models.py
+++ neo_tutorial/courses/models.py
@@ -12,13 +12,13 @@ class LessonContent(models.Model):
 
 class BasicCourse(models.Model):
     name = models.CharField(max_length=80, null=True, default='')
-    speciality = models.ForeignKey(Speciality, default=1)
+    speciality = models.ForeignKey(Speciality, on_delete=models.CASCADE, default='')
     description = models.CharField(max_length=120, null=True, default=None)
     image_url = models.CharField(max_length=120, null=True, default=None)
 
 
 class CourseMaterial(models.Model):
-    course = models.ForeignKey(BasicCourse)
+    course = models.ForeignKey(BasicCourse, on_delete=models.CASCADE, default='')
     name = models.CharField(max_length=80, null=True, default='')
     description = models.CharField(max_length=120, null=True, default=None)
     media_url = models.CharField(max_length=120, null=True, default=None)
@@ -26,15 +26,15 @@ class CourseMaterial(models.Model):
 
 
 class Lesson(models.Model):
-    course = models.ForeignKey(BasicCourse)
+    course = models.ForeignKey(BasicCourse, on_delete=models.CASCADE)
     name = models.CharField(max_length=80, null=True, default='')
     description = models.CharField(max_length=120, null=True, default=None)
     video_url = models.CharField(max_length=120, null=True, default=None)
-    content = models.ForeignKey(LessonContent)
-    test = models.ForeignKey(Test)
+    content = models.ForeignKey(LessonContent, on_delete=models.CASCADE)
+    #test = models.ForeignKey(Test, on_delete=models.CASCADE)
 
 
 class Test(models.Model):
-    lesson = models.ForeignKey(Lesson)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     name = models.CharField(max_length=80, null=True, default='')
     description = models.CharField(max_length=120, null=True, default=None)
