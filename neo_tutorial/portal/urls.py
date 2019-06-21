43c665f166e07f873368935e9c872d211ec644a1
Dmitriy Kovalyov <dmitriy.kovalyov@ddgcorp.ru>
2019-06-21 17:03:56 +0300

    Course view links

--- neo_tutorial/portal/urls.py
+++ neo_tutorial/portal/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
-from .views import CourseListView, CourseLessonView, HomeView
+from .views import CourseListView, CourseLessonView, HomeView, CourseView
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='portal-home'),
     path('courses/', CourseListView.as_view(), name='portal-courses-list'),
+    path('course/<int:id>', CourseView.as_view(), name='portal-course-view'),
     path('lesson/<int:id>/', CourseLessonView.as_view(), name='portal-lesson'),
 ]
