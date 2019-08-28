b9746ea7889613e230ef7677dbe37cddd5965454
Dmitriy Kovalyov <dmitriy.kovalyov@ddgcorp.ru>
2019-08-28 15:07:25 +0300

    Reload window with focus

--- neo_tutorial-frontend/js/main.js
+++ neo_tutorial-frontend/js/main.js
@@ -34,6 +34,31 @@
             }
         });
     });
+
+    let visibilityEvent;
+    let visibilityAttr;
+
+    if (typeof document.hidden !== 'undefined') {
+        visibilityAttr = 'hidden';
+        visibilityEvent = 'visibilitychange';
+    } else if (typeof document['msHidden'] !== 'undefined') {
+        visibilityAttr = 'msHidden';
+        visibilityEvent = 'msvisibilitychange';
+    } else if (typeof document['webkitHidden'] !== 'undefined') {
+        visibilityAttr = 'webkitHidden';
+        visibilityEvent = 'webkitvisibilitychange';
+    }
+
+    if (typeof document.addEventListener === 'undefined' || visibilityAttr === undefined) {
+        console.log('This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.');
+    } else {
+        document.addEventListener(visibilityEvent, () => {
+            if (!document[visibilityAttr]) {
+                window.location.reload();
+            }
+        }, false);
+    }
+
 })();
 
 
